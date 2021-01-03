1cdf5931376abbe8cc1823c2c7457570093944ff
Philippe Krief <philippe.krief@eclipse-foundation.org>
2021-01-03 15:23:07 +0100

    Added copyright header on  .js, .ts, .html, .css files
    Signed-off-by: Philippe Krief <philippe.krief@eclipse-foundation.org>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (C) 2021 TRIALOG
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
+
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
--- src/app/dialog-new-pia/dialog-new-pia.component.ts
+++ src/app/dialog-new-pia/dialog-new-pia.component.ts
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (C) 2021 TRIALOG
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
+
 import { Component,Input,Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
